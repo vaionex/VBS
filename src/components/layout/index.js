9968f4cd1b74f4c8e092455bd5ac48930da5c37b
Eren Guldas <erenguldas@gmail.com>
2022-02-18 10:58:06 +0300

    test layout w sidebar and header

---
+++ src/components/layout/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import TestSidebar from './test-sidebar'
+// import Header from './header'
+// import MainContent from './main-content'
+// import Sidebar from './sidebar'
+
+const Layout = () => {
+  // const [sidebarOpen, setSidebarOpen] = useState(false)
+  return (
+    // <div className="md:pl-64 flex flex-col flex-1">
+    //   <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+    //   <Header setSidebarOpen={setSidebarOpen} />
+    //   <MainContent>{children}</MainContent>
+    // </div>
+    <TestSidebar />
+  )
+}
+
+export default Layout
