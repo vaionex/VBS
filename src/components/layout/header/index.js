aa937ab9edbf3c53b3d8987bc57765806d18daae
Eren Guldas <erenguldas@gmail.com>
2022-02-18 11:25:20 +0300

    login, register buttons on header

--- src/components/layout/header/index.js
+++ src/components/layout/header/index.js
@@ -1,14 +1,7 @@
-import { Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuAlt2Icon, SearchIcon } from '@heroicons/react/outline'
-import { Fragment } from 'react'
 import PropTypes from 'prop-types'
-import cn from 'classnames'
-
-const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
-]
+import NextLink from 'next/link'
+// import UserDropdown from '@/components/ui/user-dropdown'
 
 const Header = ({ setSidebarOpen }) => {
   return (
@@ -42,6 +35,22 @@ const Header = ({ setSidebarOpen }) => {
           </form>
         </div>
         <div className="ml-4 flex items-center md:ml-6">
+          <NextLink href="/login">
+            <a className="bg-gray-200 rounded-full text-gray-900 py-2 px-6 mr-2 hover:bg-gray-300  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <span className="sr-only">Login</span>
+              {/* <BellIcon className="h-6 w-6" aria-hidden="true" /> */}
+              Login
+            </a>
+          </NextLink>
+
+          <NextLink href="/register">
+            <a className="bg-blue-700 rounded-full text-white py-2 px-6 mr-2 hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <span className="sr-only">Register</span>
+              {/* <BellIcon className="h-6 w-6" aria-hidden="true" /> */}
+              Register
+            </a>
+          </NextLink>
+
           <button
             type="button"
             className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
@@ -51,45 +60,7 @@ const Header = ({ setSidebarOpen }) => {
           </button>
 
           {/* Profile dropdown */}
-          <Menu as="div" className="ml-3 relative">
-            <div>
-              <Menu.Button className="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                <span className="sr-only">Open user menu</span>
-                <img
-                  className="h-8 w-8 rounded-full"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
-              </Menu.Button>
-            </div>
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                {userNavigation.map((item) => (
-                  <Menu.Item key={item.name}>
-                    {({ active }) => (
-                      <a
-                        href={item.href}
-                        className={cn(
-                          active ? 'bg-gray-100' : '',
-                          'block px-4 py-2 text-sm text-gray-700',
-                        )}
-                      >
-                        {item.name}
-                      </a>
-                    )}
-                  </Menu.Item>
-                ))}
-              </Menu.Items>
-            </Transition>
-          </Menu>
+          {/* <UserDropdown /> */}
         </div>
       </div>
     </header>
