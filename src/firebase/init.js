e010903d28e029ae9ac0688878b407b957489acb
Kadir Atalı <m.sametatali@gmail.com>
2022-04-03 01:58:47 +0300

    remove signout in init

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,6 +1,6 @@
 import { initializeApp, getApps } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
-import { getAuth, signOut } from 'firebase/auth'
+import { getAuth } from 'firebase/auth'
 import { getStorage } from 'firebase/storage'
 import config from './config'
 
@@ -8,12 +8,5 @@ const firebaseApp = getApps().length === 0 ? initializeApp(config) : getApps()
 const firebaseDb = getFirestore(firebaseApp)
 const firebaseAuth = getAuth(firebaseApp)
 const firebaseStorage = getStorage(firebaseApp)
-const fireBaseSignOut = async () => await signOut(firebaseAuth)
 
-export {
-  firebaseApp,
-  firebaseAuth,
-  firebaseDb,
-  firebaseStorage,
-  fireBaseSignOut,
-}
+export { firebaseApp, firebaseAuth, firebaseDb, firebaseStorage }
