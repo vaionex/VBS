8ab2ac47ec023d6c31dcb221ebe93cb6aa50c3de
Paul Spende <paul.spende@gmail.com>
2023-03-01 22:48:23 +0000

    clean repo

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['lh3.googleusercontent.com', 'firebasestorage.googleapis.com'],
-  },
-}
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-import HomeMain from '@/components/pages-partials/home'
-
-export default function HomePage() {
-  return <HomeMain />
-}
