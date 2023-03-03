3bd9c572f3641b54e570460f6d3f89a3645c3fa9
Kohze <robin@kohze.com>
2023-03-03 01:25:55 +0000

    updated home page

--- src/components/Layout/home/hero.js
+++ src/components/Layout/home/hero.js
@@ -14,7 +14,7 @@ export default function Hero() {
               </div>
               <div className="text-center">
                 <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                  Data to enrich your online business
+                  Vaionex Base Stack
                 </h1>
                 <p className="mt-6 text-lg leading-8 text-gray-600">
                   Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
