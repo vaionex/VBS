00a533e7fc094dbda8444bfb0bf5d1334bbeb98f
Paul Spende <paul.spende@gmail.com>
2023-04-19 15:53:08 +0100

    Add an update new firebase functionalities

--- src/firebase/app.js
+++ src/firebase/app.js
@@ -1,20 +1,19 @@
 import { initializeApp } from 'firebase/app'
 
-const {
-  FIREBASE_API_KEY,
-  FIREBASE_AUTH_DOMAIN,
-  FIREBASE_DATABASE_URL,
-  FIREBASE_STORAGE_BUCKET,
-  FIREBASE_MESSAGING_SENDER_ID,
-} = process.env
+const apiKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY
+const authDomain = process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN
+const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+const databaseURL = process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL
+const storageBucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET
+const messagingSenderId = process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
 
-// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY, // Auth / General Use
-  authDomain: FIREBASE_AUTH_DOMAIN, // Auth with popup/redirect
-  databaseURL: FIREBASE_DATABASE_URL, // Realtime Database
-  storageBucket: FIREBASE_STORAGE_BUCKET, // Storage
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID, // Cloud Messaging
+  apiKey, // Auth / General Use
+  authDomain, // Auth with popup/redirect
+  projectId, // General Use
+  databaseURL, // Realtime Database
+  storageBucket, // Storage
+  messagingSenderId, // Cloud Messaging
 }
 
-export const app = initializeApp(firebaseConfig)
+export const firebase = initializeApp(firebaseConfig)
