dc1235cdc3278d16f879230c0f03a919f6315780
kashif-vaionex <kashif@vaionex.com>
2023-05-16 15:48:16 +0500

    CU-866a9cgwg - Add Novu to VBS

---
+++ src/app/api/novu/notification/route.js
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server"
+import { Novu } from "@novu/node"
+
+const novu = new Novu(process.env.NEXT_PUBLIC_NOVU_API_KEY)
+
+export async function POST(request, params) {
+  const { userId, message } = await request.json()
+  try {
+    // notifcation template Id 
+    await novu.trigger('on-boarding-notification', {
+      to: {
+        subscriberId: userId,
+      },
+      payload: {
+        notificationContent: message,
+      },
+    })
+      .catch((err) => {
+        console.log('in catch: ', err)
+        return NextResponse.json({ status: 'Error', err })
+      })
+  } catch (error) {
+    console.error('error::', error)
+    return NextResponse.json({ status: 'error', error })
+  }
+}
---
+++ src/app/api/novu/subscriber/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server"
+import { Novu } from "@novu/node"
+
+const novu = new Novu(process.env.NEXT_PUBLIC_NOVU_API_KEY)
+
+export async function POST(request, params) {
+  const { userId, userEmail, userName } = await request.json()
+
+  try {
+    await novu.subscribers.identify(userId, {
+      email: userEmail,
+      firstName: userName,
+    })
+      .catch((err) => {
+        console.log('in catch: ', err)
+        return NextResponse.json({ status: 'Error', err })
+      })
+
+  } catch (error) {
+    console.error('error::', error)
+    return NextResponse.json({ status: 'error', error })
+  }
+}
